c408f4f43b929487059a7c817c1ea3170eb13ac4
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-01-31 00:33:37 +0530

    Add files via upload

---
+++ Problem_11.java
@@ -0,0 +1,49 @@
+/*
+
+Problem: 11
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+*/
+
+import java.util.*;
+
+public class Problem_11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("Enetr the array size: ");
+        n=sc.nextInt();
+        int[] a=new int[n];
+        System.out.println("Enetr the elements: ");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Original array is: "+Arrays.toString(a));
+        Arrays.sort(a);
+        for(i=0;i<n;i++){
+            if(a[i] != i){
+                System.out.println("Missing element is: "+i);
+                return;
+            }
+        }
+        System.out.println("Missing element is: "+n);
+    }
+}

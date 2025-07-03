8cb29460ece9595674e42af385b88ca0917cf693
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-07-03 21:20:39 +0530

    Add files via upload

---
+++ Problem_81.java
@@ -0,0 +1,52 @@
+/*
+
+Problem: 81
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+ */
+
+import java.util.HashMap;
+
+public class Problem_81 {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int k1 = 3;
+        System.out.println(containsNearbyDuplicate(nums1, k1));
+
+        int[] nums2 = {1, 0, 1, 1};
+        int k2 = 1;
+        System.out.println(containsNearbyDuplicate(nums2, k2));
+
+        int[] nums3 = {1, 2, 3, 1, 2, 3};
+        int k3 = 2;
+        System.out.println(containsNearbyDuplicate(nums3, k3));
+    }
+}
